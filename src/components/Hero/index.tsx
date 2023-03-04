3c982f5d4e1ac0219857ed4bddaa7e2d2d90400b
DanteCam <76830701+DanteCam@users.noreply.github.com>
2023-03-04 17:20:33 -0600

    First commit: project created, style technologies added.

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,12 @@
+
+export default function Hero() {
+    return <section className="w-full min-h-screen h-auto">
+        <div className="flex w-full justify-center h-full py-10">
+            <div className= 'w-fit flex items-center justify-center'>
+                <h1 className="text-[5rem] capitalize font-bold">
+                    {'Dante Camacho'}
+                </h1>
+            </div>
+        </div>
+    </section>
+}
